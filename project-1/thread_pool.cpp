44301964660678b813dfac7c90ceec8329945c11
Rasmus <ahtavarasmus@gmail.com>
2023-05-02 19:12:19 +0300

    comments for threadpool constructor

--- project-1/thread_pool.cpp
+++ project-1/thread_pool.cpp
@@ -1,31 +1,43 @@
 #include "thread_pool.hh"
 
 ThreadPool::ThreadPool(size_t num_threads) : stop(false) {
+    // Iterate through the number of requested threads
     for (size_t i = 0; i < num_threads; ++i) {
+        // Create a new thread, with a lambda function that captures 'this' pointer of ThreadPool object
         workers.emplace_back([this] {
+            // This loop continues until 'stop' is set to true and there are no more tasks in the queue
             while (true) {
                 std::function<void()> task;
 
+                // Lock the tasks_mutex to access the tasks queue
                 {
                     std::unique_lock<std::mutex> lock(this->tasks_mutex);
+                    // Wait for a new task to be added to the queue, or for the 'stop' flag to be set
                     this->tasks_cv.wait(lock, [this] {
                         return this->stop || !this->tasks.empty();
                     });
 
+                    // If 'stop' is set and there are no more tasks, break out of the loop
                     if (this->stop && this->tasks.empty()) {
                         return;
                     }
 
+                    // Retrieve the next task from the queue and remove it
                     task = std::move(this->tasks.front());
                     this->tasks.pop();
+                    // Increment the count of active tasks
                     this->active_tasks++;
                 }
 
+                // Execute the task
                 task();
 
+                // Lock the tasks_mutex again to update the active_tasks count and notify 'completed_cv'
                 {
                     std::unique_lock<std::mutex> lock(this->tasks_mutex);
+                    // Decrement the count of active tasks
                     this->active_tasks--;
+                    // Notify all waiting threads that a task has completed
                     this->completed_cv.notify_all();
                 }
             }
